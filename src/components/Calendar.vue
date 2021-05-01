c15b10806a0c846ce75bb94c6839d4fca1452592
joffrey <joffrey.berrier@gmail.com>
2021-05-01 12:31:17 +0200

    add pagination

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,10 +1,39 @@
 <template>
-  <div>
-    <p>{{checkIn}} - {{checkOut}}</p>
-    <div class="px-4 grid grid-cols-2 gap-4">
+  <div class="px-4">
+    <p class="py-4 h-20">
+      <template v-if="checkIn">
+        {{ checkIn }} - {{ checkOut }}
+      </template>
+    </p>
+
+    <div class="relative grid grid-cols-2 items-center gap-4">
+      <button
+        :disabled="activeIndex === 0"
+        @click="activeIndex--"
+        class="absolute left-0 w-10 h-10 flex items-center justify-center border border-gray-200 focus:outline-none disabled:opacity-50"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 stroke-current text-gray-800" fill="none" viewBox="0 0 24 24">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
+        </svg>
+      </button>
+
+      <p class="text-center py-2">{{ months[activeIndex].monthName }}</p>
+      <p class="text-center py-2">{{ months[activeIndex + 1].monthName }}</p>
+
+      <button
+        :disabled="activeIndex >= months.length - 2"
+        @click="activeIndex++"
+        class="absolute right-0 w-10 h-10 flex items-center justify-center border border-gray-200 focus:outline-none disabled:opacity-50"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 stroke-current text-gray-800" fill="none" viewBox="0 0 24 24">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+        </svg>
+      </button>
+    </div>
+
+    <div class="grid grid-cols-2 gap-4">
       <div v-for="month in paginate">
-        <div class="text-center">{{ month.monthName }}</div>
-        <ul class="grid grid-cols-7 text-center py-4">
+        <ul class="grid grid-cols-7 text-center py-6 text-sm">
           <li>Mon</li>
           <li>Tue</li>
           <li>Wed</li>
@@ -17,7 +46,7 @@
           <button
             type="button"
             :class="[
-              'focus:outline-none relative pb-[100%] border border-gray-200 transition-all',
+              'focus:outline-none relative pb-[100%] border border-gray-200',
               { 'pointer-events-none text-gray-300 line-through': !day.belongsToThisMonth },
               { 'border-2 border-green-500' : formatToday === day.formatDay },
               { 'bg-green-500' : checkIn === day.date },
@@ -74,6 +103,7 @@ export default defineComponent({
   },
   data() {
     return {
+      activeIndex: 0 as number,
       today: new Date as Date,
       disabledDates: [] as string[],
       hoveringDates: [] as string[],
@@ -95,7 +125,7 @@ export default defineComponent({
       return format(this.today, "DD/MM/YYYY")
     },
     paginate(): Month[] {
-      return this.months.slice(0, 2)
+      return this.months.slice(this.activeIndex, 2 + this.activeIndex)
     },
   },
   methods: {
