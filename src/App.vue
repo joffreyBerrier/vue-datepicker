c95a4dcad67463498a76aefacba574fa0a40cbb8
Joffrey Berrier <joffreyberrier@macbook-pro-de-joffrey-1.home>
2022-05-17 15:56:06 +0200

    add monday period

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import Calendar from "./components/Calendar.vue";
 import type { Ref } from "vue";
 import type { Booking, Period } from "./types";
 
-const bookedDates = ref([]);
+const bookedDates = ref(["2022-05-13", "2022-05-12"]);
 const bookingColor = ref({
   admin: "#9dc1c9",
   contract: "#a56a0b",
@@ -27,7 +27,104 @@ const bookingDates: Ref<Booking[]> = ref([
     type: "contract",
   },
 ]);
-const periodDates: Ref<Period[]> = ref([]);
+const periodDates: Ref<Period[]> = ref([
+  {
+    startAt: "2022-08-06",
+    endAt: "2022-09-10",
+    periodType: "weekly_by_saturday",
+    minimumDuration: 2,
+  },
+  {
+    startAt: "2022-09-10",
+    endAt: "2022-10-01",
+    periodType: "weekly_by_saturday",
+    minimumDuration: 2,
+  },
+  {
+    startAt: "2022-10-08",
+    endAt: "2022-10-22",
+    periodType: "weekly_by_saturday",
+    minimumDuration: 2,
+  },
+  {
+    startAt: "2022-10-22",
+    endAt: "2022-11-26",
+    periodType: "weekly_by_saturday",
+    minimumDuration: 3,
+  },
+  {
+    startAt: "2022-12-18",
+    endAt: "2023-01-01",
+    periodType: "weekly_by_sunday",
+    minimumDuration: 1,
+  },
+  {
+    startAt: "2023-01-01",
+    endAt: "2023-01-05",
+    periodType: "nightly",
+    minimumDuration: 3,
+  },
+  {
+    startAt: "2023-01-05",
+    endAt: "2023-01-15",
+    periodType: "nightly",
+    minimumDuration: 7,
+  },
+  {
+    startAt: "2023-01-15",
+    endAt: "2023-01-29",
+    periodType: "weekly_by_sunday",
+    minimumDuration: 1,
+  },
+  {
+    startAt: "2023-01-29",
+    endAt: "2023-02-26",
+    periodType: "nightly",
+    minimumDuration: 10,
+  },
+  {
+    startAt: "2023-02-26",
+    endAt: "2023-03-05",
+    periodType: "weekly_by_sunday",
+    minimumDuration: 1,
+  },
+  {
+    startAt: "2023-03-11",
+    endAt: "2023-04-15",
+    periodType: "weekly_by_saturday",
+    minimumDuration: 3,
+  },
+  {
+    startAt: "2023-04-16",
+    endAt: "2023-05-21",
+    periodType: "weekly_by_sunday",
+    minimumDuration: 1,
+  },
+  {
+    startAt: "2023-05-21",
+    endAt: "2023-05-25",
+    periodType: "nightly",
+    minimumDuration: 2,
+  },
+  {
+    startAt: "2023-05-25",
+    endAt: "2023-05-28",
+    periodType: "nightly",
+    minimumDuration: 3,
+  },
+  {
+    startAt: "2023-05-28",
+    endAt: "2023-06-04",
+    periodType: "nightly",
+    minimumDuration: 7,
+  },
+  {
+    startAt: "2023-11-27",
+    endAt: "2023-12-25",
+    periodType: "weekly_by_monday",
+    minimumDuration: 1,
+  },
+]);
 const checkIn = ref(null);
 const checkOut = ref(null);
 const nextBookedDates: Ref<Booking[]> = ref([]);
